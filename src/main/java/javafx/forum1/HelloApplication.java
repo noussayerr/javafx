f8e9dfe5bd7798ce77564db65537e75a7f4ab522
Aziz <kthiriaziz149@gmail.com>
2026-04-15 02:46:44 +0100

    css

--- src/main/java/javafx/forum1/HelloApplication.java
+++ src/main/java/javafx/forum1/HelloApplication.java
@@ -6,13 +6,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 820, 560);
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+
+        URL stylesheet = HelloApplication.class.getResource("app.css");
+        if (stylesheet != null) {
+            scene.getStylesheets().add(stylesheet.toExternalForm());
+        }
+
         stage.setTitle("Forum1 - Gestion des postes");
+        stage.setMinWidth(920);
+        stage.setMinHeight(620);
         stage.setScene(scene);
         stage.show();
     }
